01b336c2ef83b80597d84381a407c237f891fe2d
Krastan Draganov <krastan.draganov@gmail.com>
2020-02-11 22:49:42 +0200

    Trying to solve routes

--- routes.cpp
+++ routes.cpp
@@ -6,12 +6,32 @@
 using namespace std;
 const int MAXN=3e5+3,MAXM=7e5+3,INF=1e9;
 vector<pair<int,int>> graph[MAXN];
-int bfs[MAXM];
+int bfs[MAXM],parent[MAXN],depth[MAXN];
+int find_parent(int curr){
+    if(curr==parent[curr]){
+        return curr;
+    }
+    return parent[curr]=find_parent(parent[curr]);
+}
+void union_vertex(int num1, int num2){
+    int parent1=find_parent(num1),parent2=find_parent(num2);
+    if(depth[parent1]!=depth[parent2]){
+        if(depth[parent2]>depth[parent1]){
+            swap(parent1,parent2);
+        }
+        depth[parent1]+=depth[parent2];
+        parent[parent2]=parent1;
+    }
+}
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    //ios::sync_with_stdio(false);
+    //cin.tie(nullptr);
     int n,m;
     cin>>n>>m;
+    for(int i=0;i<n;++i){
+        parent[i]=i;
+        depth[i]=1;
+    }
     for(int i=0;i<m;++i){
         int from,to,dtime;
         cin>>from>>to>>dtime;
@@ -19,23 +39,32 @@ int main(){
         --to;
         graph[from].push_back({to,dtime});
         graph[to].push_back({from,dtime});
+        if(!dtime){
+            union_vertex(from,to);
+        }
     }
-    vector<int> dist(n,INF);
+    for(int i=0;i<n;++i){
+        cout<<i<<" "<<find_parent(i)<<endl;
+    }
+    vector<int> dist(n,-1);
     int bfs_start=0,bfs_end=0;
-    bfs[bfs_end++]=0;
-    dist[0]=0;
-    while(bfs_end-bfs_start>0){
-        int curr=bfs[bfs_start++];
+    priority_queue<int> bfs;
+    bfs.push(0);
+    dist[find_parent(0)]=0;
+    while(!bfs.empty()){
+        int curr=bfs.top();
+        bfs.pop();
+        //cout<<curr<<endl;
         for(pair<int,int> nextv : graph[curr]){
-            int curr_dist=dist[curr]+nextv.second;
-            if(dist[nextv.first]>curr_dist){
-                dist[nextv.first]=curr_dist;
-                bfs[bfs_end++]=nextv.first;
+            if(dist[find_parent(nextv.first)]==-1){
+                dist[find_parent(nextv.first)]=dist[find_parent(curr)]+1;
+                bfs.push(nextv.first);
             }
         }
     }
     for(int i=0;i<n;++i){
-        cout<<(dist[i]==INF ? -1 : dist[i])<<" ";
+        int curr=dist[find_parent(i)];
+        cout<<(curr==INF ? -1 : curr)<<" ";
     }
     cout<<endl;
 return 0;
