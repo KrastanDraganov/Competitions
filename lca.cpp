ab7ecae4abebdfa0bfe49fe77794ef3a6d49ad04
Krastan Draganov <krastan.draganov@gmail.com>
2020-05-21 23:17:27 +0300

    Solving LCA using Euler tour and RMQ

--- lca.cpp
+++ lca.cpp
@@ -1,96 +1,113 @@
 #include<iostream>
 #include<vector>
 #include<utility>
-#include<queue>
-#include<cstring>
 
 #define endl '\n'
 
 using namespace std;
 
-const int MAXN=5e3+3;
-vector<pair<int,int>> graph[MAXN];
-int depth[MAXN],lca[MAXN][32];
-long long dist[MAXN];
+const int MAXN=1003,MAXL=20;
+vector<int> graph[MAXN];
+vector<pair<int,int>> euler;
+pair<int,int> rmq[3*MAXN][MAXL];
+int first_meet[MAXN],log[3*MAXN];
+bool is_child[MAXN];
 
-void calc_bfs(){
-    queue<int> bfs;
-    bfs.push(0);
-    memset(depth,-1,sizeof(depth));
-    depth[0]=0;
-    lca[0][0]=0;
-    while(!bfs.empty()){
-        int curr=bfs.front();
-        bfs.pop();
-        for(pair<int,int> nextv : graph[curr]){
-            if(depth[nextv.first]==-1){
-                depth[nextv.first]=depth[curr]+1;
-                dist[nextv.first]=dist[curr]+nextv.second;
-                lca[nextv.first][0]=curr;
-                bfs.push(nextv.first);
-            }
+void dfs(int currv, int parent, int depth){
+    euler.push_back({depth,currv});
+    if(first_meet[currv]==-1){
+        first_meet[currv]=(int)euler.size()-1;
+    }
+    for(int nextv : graph[currv]){
+        if(nextv!=parent){
+            dfs(nextv,currv,depth+1);
+            euler.push_back({depth,currv});
         }
     }
 }
 
-void calc_lca(int max_power, int n){
-    for(int i=1;i<=n;++i){
+void calc_rmq(int n){
+    for(int i=0;i<n;++i){
+        rmq[i][0]=euler[i];
+    }
+    for(int i=1;i<MAXL;++i){
         for(int i2=0;i2<n;++i2){
-            lca[i2][i]=lca[lca[i2][i-1]][i-1];
+            int i3=i2+(1<<(i-1));
+            rmq[i2][i]=(i3<n ? min(rmq[i2][i-1],rmq[i3][i-1]) : rmq[i2][i-1]);
         }
     }
 }
 
-int find_lca(int x, int y, int max_power){
-    if(depth[x]<depth[y]){
+int find_lca(int x, int y){
+    x=first_meet[x];
+    y=first_meet[y];
+    if(x>y){
         swap(x,y);
     }
-    int diff=depth[x]-depth[y];
-    for(int i=max_power;diff>0;--i){
-        if(diff%2==1){
-            x=lca[x][i];
-        }
-        diff/=2;
+    int diff=y-x+1;
+    if(diff==1){
+        return rmq[x][0].second;
     }
-    if(x==y){
-        return x;
-    }
-    for(int i=max_power;i>=0;--i){
-        if(lca[x][i]!=lca[y][i]){
-            x=lca[x][i];
-            y=lca[y][i];
-        }
-    }
-    return lca[x][0];
+    int curr_log=log[diff];
+    return min(rmq[x][curr_log-1],rmq[y-(1<<(curr_log-1))+1][curr_log-1]).second;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n,q;
-    cin>>n>>q;
-    for(int i=1;i<n;++i){
-        int from,to,weight;
-        cin>>from>>to>>weight;
-        --from;
-        --to;
-        graph[from].push_back({to,weight});
-        graph[to].push_back({from,weight});
+
+    log[1]=0;
+    for(int i=2;i<3*MAXN;++i){
+        log[i]=log[i/2]+1;
     }
-    int max_power=0;
-    while((1<<max_power)<=n){
-        ++max_power;
-    }
-    --max_power;
-    calc_bfs();
-    calc_lca(max_power,n);
-    while(q--){
-        int from,to;
-        cin>>from>>to;
-        --from;
-        --to;
-        int curr_lca=find_lca(from,to,max_power);
-        cout<<dist[from]+dist[to]-2*dist[curr_lca]<<endl;
+    int tests;
+    cin>>tests;
+    for(int test=1;test<=tests;++test){
+        int n;
+        cin>>n;
+        euler.clear();
+        for(int i=0;i<n;++i){
+            graph[i].clear();
+            is_child[i]=false;
+            first_meet[i]=-1;
+        }
+        for(int i=0;i<3*MAXN;++i){
+            for(int i2=0;i2<MAXL;++i2){
+                rmq[i][i2]={1e9,1e9};
+            }
+        }
+        for(int i=0;i<n;++i){
+            int m;
+            cin>>m;
+            while(m--){
+                int child;
+                cin>>child;
+                --child;
+                graph[i].push_back(child);
+                is_child[child]=true;
+            }
+        }
+        
+        int root=-1;
+        for(int i=0;i<n;++i){
+            if(!is_child[i]){
+                root=i;
+                break;
+            }
+        }
+        dfs(root,-1,0);
+        calc_rmq((int)euler.size());
+
+        int q;
+        cin>>q;
+        cout<<"Case "<<test<<":\n";
+        while(q--){
+            int x,y;
+            cin>>x>>y;
+            --x;
+            --y;
+            cout<<find_lca(x,y)+1<<endl;
+        }
     }
 return 0;
 }
