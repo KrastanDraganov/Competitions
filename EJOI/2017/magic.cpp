0a8330e7ba25c78186cc728c00fa744daabe990c
Krastan Draganov <krastan.draganov@gmail.com>
2020-09-25 23:00:33 +0300

    Solving EJOI 2017 problem Magic with Hashing

--- EJOI/2017/magic.cpp
+++ EJOI/2017/magic.cpp
@@ -7,10 +7,9 @@
 
 using namespace std;
 
-const int MAXN1=2e3+3,MAXN2=1e5+3,ALPHABET=2*26,MOD=1e9+7;
+const int MAXN=1e5+3,ALPHABET=2*26,BASE=31,MOD=1e9+7;
 
-int counter[MAXN1][ALPHABET];
-int prefix[MAXN2];
+int prefix[ALPHABET][MAXN];
 
 void add_mod(int& x, int y){
     x+=y;
@@ -19,63 +18,66 @@ void add_mod(int& x, int y){
     }
 }
 
-int letter_code(char letter){
-    if(letter>='a' and letter<='z'){
-        return letter-'a'+1;
+void calc_prefix(int ind, char letter1, char letter2, int n, string& s){
+    for(int i=0;i<n;++i){
+        int curr=0;
+        
+        if(s[i]==letter1){
+            curr=1;
+        }else if(s[i]==letter2){
+            curr=-1;
+        }
+        
+        prefix[ind][i+1]=prefix[ind][i]+curr;
     }
-    return letter-'A'+26+1;
 }
 
-int subtask2(int n, string& s){
-    set<int> all_letters;
-    for(int i=0;i<n;++i){
-        int curr_code=letter_code(s[i]);
-
-        for(int i2=0;i2<ALPHABET;++i2){
-            counter[i+1][i2]=counter[i][i2];
-        }
-        ++counter[i+1][curr_code-1];
-
-        all_letters.insert(curr_code);
-    }
-
-    int res=0;
-    int k=(int) all_letters.size();
-
-    for(int len=k;len<=n;len+=k){
-        int curr=0;
-        int target=len/k;
-        
-        for(int i=0;i+len-1<n;++i){
-
-            bool is_magic=true;
-            for(int i2=0;i2<ALPHABET;++i2){
-                int diff=counter[i+len][i2]-counter[i][i2];
-                is_magic &= (!diff or diff==target);
-            }
-
-            curr+=is_magic;
-        }
-        
-        add_mod(res,curr);
-    }
+unsigned long long make_hash(int ind, int k){
+    unsigned long long res=0;
     
+    for(int i=0;i<k-1;++i){
+        res=res*BASE+prefix[i][ind]+1;
+    }
+
     return res;
 }
 
-int subtask3(int n, string& s){
-    unordered_map<int,int> prev;
-    ++prev[0];
+int magic_substrings(int n, string& s){
+    set<char> all_letters;
+    for(char curr : s){
+        all_letters.insert(curr);
+    }
 
-    char target=s[0];
-    prefix[1]=1;
-    ++prev[1];
+    int k=(int) all_letters.size();
+    if(k==1){
+        return n;
+    }
+
+    auto it=all_letters.begin();
+    char letter1=*it;
+    ++it;
+
+    for(int i=0; it!=all_letters.end() ;++i){
+        char letter2=*it;
+        ++it;
+
+        calc_prefix(i, letter1, letter2, n, s);
+
+        letter1=letter2;
+    }
+
+    unordered_map<unsigned long long, int> prev;
+    unsigned long long start_hash=0;
+    for(int i=0;i<k-1;++i){
+        start_hash=start_hash*BASE+1;
+    }
+    ++prev[start_hash];
 
     int res=0;
-    for(int i=1;i<n;++i){
-        prefix[i+1]=prefix[i]+(target==s[i] ? 1 : -1);
-        add_mod(res,prev[prefix[i+1]]);
-        ++prev[prefix[i+1]];
+    for(int i=0;i<n;++i){
+        unsigned long long curr_hash=make_hash(i+1,k);
+        add_mod(res,prev[curr_hash]);
+        ++prev[curr_hash];
     }
 
     return res;
@@ -89,11 +91,6 @@ int main(){
     string s;
     cin>>n>>s;
 
-    
-    if(n<MAXN1){
-        cout<<subtask2(n,s)<<endl;
-    }else{
-        cout<<subtask3(n,s)<<endl;
-    }
+    cout<<magic_substrings(n,s)<<endl;
 return 0;
 }
