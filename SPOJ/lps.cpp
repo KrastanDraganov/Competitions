1078b064eb84216be27ff1df4e14b6307efd1099
Krastan Draganov <krastan.draganov@gmail.com>
2020-10-15 23:39:16 +0300

    Successfully debugged SPOJ LPS problem

--- SPOJ/lps.cpp
+++ SPOJ/lps.cpp
@@ -1,5 +1,3 @@
-// Not solved - wrong answer
-
 #include<iostream>
  
 #define endl '\n'
@@ -7,43 +5,36 @@
 using namespace std;
  
 const int MAXN=1e5+3,BASE=31;
-unsigned long long pow_s[MAXN],hash_s[2][MAXN];
-
-unsigned long long left_hash(int ind, int len){
-    return hash_s[0][ind]-hash_s[0][ind-len];
+uint64_t pow_s[MAXN],hash_s[2][MAXN];
+ 
+uint64_t left_hash(int ind, int len, int n){
+    return pow_s[n-ind+len]*(hash_s[0][ind]-hash_s[0][ind-len]);
 }
-
-unsigned long long right_hash(int ind, int len){
-    return hash_s[1][ind]-hash_s[1][ind+len];
+ 
+uint64_t right_hash(int ind, int len, int n){
+    return pow_s[ind+len-1]*(hash_s[1][ind]-hash_s[1][ind+len]);
 }
  
 int calc_pal(int ind, int n, bool is_even){
-    int rind=n-ind-1;
-    ind-=is_even;
-    // cout<<"state: "<<ind<<" "<<rind<<endl;
-    // if(rind<ind){
-    //     return 0;
-    // }
-
+    int rind=n-ind-1-is_even;
     int l=0,r=min(ind, rind);
+ 
     while(l<=r){
         int mid=(l+r)/2;
-        // cout<<"search: "<<l<<" "<<r<<" "<<mid+1<<endl;
-        // cout<<"hashes: "<<left_hash(ind+1, mid+1)<<" "<<right_hash(rind+1, mid+1)<<endl;
-        if(left_hash(ind+1, mid+1)==right_hash(rind+1, mid+1)){
+        if(left_hash(ind+1, mid+1, n)==right_hash(ind+is_even+1, mid+1, n)){
             l=mid+1;
         }else{
             r=mid-1;
         }
     }
  
-    return (r<0 ? 0 : r);
+    return (l<1 ? 0 : l-1);
 }
  
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+ 
     int n;
     string s;
     cin>>n>>s;
@@ -61,12 +52,9 @@ int main(){
     int res=1;
     for(int i=1;i<n;++i){
         int curr=2*calc_pal(i, n, false)+1;
-        // cout<<"odd: "<<curr<<endl;
         res=max(res, curr);
-        
         if(s[i]==s[i-1]){
-            curr=2*calc_pal(i, n, true)+2;
-            // cout<<"even: "<<curr<<endl;
+            curr=2*calc_pal(i-1, n, true)+2;
             res=max(res, curr);
         }
     }
