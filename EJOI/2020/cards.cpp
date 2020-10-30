575e7ab1abb9c638fae250922205f9c7f252fda7
Krastan Draganov <krastan.draganov@gmail.com>
2020-10-30 13:40:23 +0200

    Debugging problem Cards from EJOI 2020

--- EJOI/2020/cards.cpp
+++ EJOI/2020/cards.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include<vector>
 
 #include "cards.h"
@@ -7,28 +8,26 @@ using namespace std;
 void swapCards(int cards[], int s, int t){
     int total_swaps=0;
     for(int i=0;i<52 and total_swaps<s;++i){
-        if(cards[i]!=-1){
-            int pos=i;
-            vector<int> cycle;
-            
-            while(cards[pos]!=-1){
-                cycle.push_back(pos);
-                int curr=pos;
-                pos=cards[pos];
-                cards[curr]=-1;
-            }
+        int pos=i;
+        vector<int> cycle;
+        
+        while(cards[pos]!=-1){
+            cycle.push_back(pos);
+            int curr=pos;
+            pos=cards[pos];
+            cards[curr]=-1;
+        }
 
-            int n=(int) cycle.size();
-            if(t<n){
-                doSwap(cycle[n/2], i);
-                ++total_swaps;
-            }
+        int n=(int) cycle.size();
+        for(int limit=t;limit<n and total_swaps<s;limit+=t){
+            doSwap(cycle[0], cycle[limit]);
+            ++total_swaps;
         }
     }
 }
 
 void guessCard(int s, int t, int target){
-    while(true){
+    while(t--){
         int i=guess(target);
         target=i;
     }
