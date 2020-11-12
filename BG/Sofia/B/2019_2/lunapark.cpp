b6df25238a32479e252a1a130c761fef8ee037ac
Krastan Draganov <krastan.draganov@gmail.com>
2020-11-12 22:50:56 +0200

    Debugging BG Sofia B 2019_2 problem Lunapark

--- BG/Sofia/B/2019_2/lunapark.cpp
+++ BG/Sofia/B/2019_2/lunapark.cpp
@@ -1,5 +1,3 @@
-//Only 0/100 points
-
 #include<iostream>
 #include<vector>
 #include<stack>
@@ -9,58 +7,64 @@
 
 using namespace std;
 
-const int MAXN=2e5+3;
-vector<pair<int,int>> graph[MAXN],cycle;
+const int MAXN=2e5+3,MAXL=20;
+vector<pair<int,int>> graph[MAXN];
 bool used[MAXN];
-int orig_pos[MAXN],cycle_pos[MAXN],root_dist[MAXN],logarithm[MAXN],rmq[MAXN][20];
-stack<pair<int,int>> path;
+int logarithm[MAXN],rmq[MAXN][MAXL];
+int initial_pos[MAXN],cycle_pos[MAXN],root_dist[MAXN];
 
-bool dfs1(int curr, int parent){
-    used[curr]=true;
-    for(pair<int,int> next : graph[curr]){
-        if(next.first==parent){
+bool dfs1(int currv, int parent, stack<pair<int, int>>& path, vector<pair<int, int>>& cycle){
+    used[currv]=true;
+    
+    for(pair<int,int> nextv : graph[currv]){
+        if(nextv.first==parent){
             continue;
         }
-        if(used[next.first]){
-            cycle.push_back(next);
-            while(path.top().first!=next.first){
+        
+        if(used[nextv.first]){
+            cycle.push_back(nextv);
+            while(path.top().first!=nextv.first){
                 cycle.push_back(path.top());
                 path.pop();
             }
             return true;
         }
-        path.push(next);
-        if(dfs1(next.first,curr)){
+        
+        path.push(nextv);
+        if(dfs1(nextv.first, currv, path, cycle)){
             return true;
         }
         path.pop();
     }
+    
     return false;
 }
 
-void dfs2(int curr){
-    used[curr]=true;
-    for(pair<int,int> next : graph[curr]){
-        if(!used[next.first]){
-            root_dist[next.first]=max(next.second,root_dist[curr]);
-            cycle_pos[next.first]=cycle_pos[curr];
-            dfs2(next.first);
+void dfs2(int currv){
+    used[currv]=true;
+    for(pair<int,int> nextv : graph[currv]){
+        if(!used[nextv.first]){
+            root_dist[nextv.first]=max(nextv.second, root_dist[currv]);
+            cycle_pos[nextv.first]=cycle_pos[currv];
+            dfs2(nextv.first);
         }
     }
 }
 
-void calc_rmq(int n){
+void calc_rmq(vector<pair<int, int>>& cycle){
     logarithm[1]=0;
-    for(int i=2;i<=n;++i){
+    for(int i=2;i<MAXN;++i){
         logarithm[i]=logarithm[i/2]+1;
     }
-    for(int i=0;i<cycle.size();++i){
+    
+    int n=(int) cycle.size();
+    for(int i=0;i<n;++i){
         rmq[i][0]=cycle[i].second;
     }
-    for(int i=1;i<20;++i){
-        for(int i2=0;i2<cycle.size();++i2){
-            int i3=i2+(1<<(i-1));
-            rmq[i2][i]=(i3<cycle.size() ? max(rmq[i2][i-1],rmq[i3][i-1]) : rmq[i2][i-1]);
+    
+    for(int range=1;range<MAXL;++range){
+        for(int i=0;i+(1<<range)<=n;++i){
+            rmq[i][range]=max(rmq[i][range-1], rmq[i+(1<<(range-1))][range-1]);
         }
     }
 }
@@ -69,11 +73,8 @@ long long query(int l, int r){
     if(l>r){
         return 0;
     }
-    int diff=r-l+1;
-    if(diff==1){
-        return rmq[l][0];
-    }
-    return max(rmq[l][logarithm[diff]-1],rmq[r-(1<<(logarithm[diff]-1))+1][logarithm[diff]-1]);
+    int range=logarithm[r-l+1];
+    return max(rmq[l][range], rmq[r-(1<<range)+1][range]);
 }
 
 int main(){
@@ -85,53 +86,65 @@ int main(){
     for(int i=0;i<n;++i){
         int from,to,weight;
         cin>>from>>to>>weight;
-        graph[from].push_back({to,weight});
-        graph[to].push_back({from,weight});
+        --from;
+        --to;
+        graph[from].push_back({to, weight});
+        graph[to].push_back({from, weight});
     }
     
-    path.push({1,0});
-    dfs1(1,0);
+    vector<pair<int, int>> cycle;
+    stack<pair<int, int>> path;
+    path.push({0, 0});
+    dfs1(0, -1, path, cycle);
 
-    for(int i=1;i<=n;++i){
-        orig_pos[i]=i;
+    for(int i=0;i<n;++i){
+        initial_pos[i]=i;
         used[i]=false;
     }
-    
-    for(int i=0;i<cycle.size();++i){
+
+    int m=(int) cycle.size();
+    for(int i=0;i<m;++i){
         cycle_pos[cycle[i].first]=i;
         used[cycle[i].first]=true;
-    
     }
     for(pair<int,int> curr : cycle){
         dfs2(curr.first);
     }
-    calc_rmq(n);
+    calc_rmq(cycle);
     
     int q;
     cin>>q;
-    long long ans=0;
-    for(int i=0;i<q;++i){
+    
+    long long res=0;
+    while(q--){
         int type,x,y;
         cin>>type>>x>>y;
-        if(type==1){
-            x=orig_pos[x];
-            y=orig_pos[y];
-            long long max_dist=max(root_dist[x],root_dist[y]);
-            x=cycle_pos[x];
-            y=cycle_pos[y];
-            if(x>y){
-                swap(x,y);
-            }
-            long long res1=max(max_dist,query(x,y-1));
-            long long res2=max(max_dist,max(query(0,x-1),query(y,cycle.size()-1)));
-            if(res2>res1){
-                swap(res1,res2);
-            }
-            ans+=2ll*res1+res2;
-        }else{
-            swap(orig_pos[x],orig_pos[y]);
+        --x;
+        --y;
+
+        if(type==2){
+            swap(initial_pos[x], initial_pos[y]);
+            continue;
         }
+        
+        x=initial_pos[x];
+        y=initial_pos[y];
+        long long max_dist=max(root_dist[x], root_dist[y]);
+        
+        x=cycle_pos[x];
+        y=cycle_pos[y];
+        if(x>y){
+            swap(x,y);
+        }
+        long long res1=max(max_dist, query(x, y-1));
+        long long res2=max(max_dist, max(query(0, x-1), query(y, m-1)));
+        
+        if(res2>res1){
+            swap(res1,res2);
+        }
+        res+=2*res1+res2;
     }
-    cout<<ans<<endl;
+    
+    cout<<res<<endl;
 return 0;
 }
