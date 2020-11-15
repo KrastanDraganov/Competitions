46155b0fe5a6ff15924eefcc1217491603b31497
Krastan Draganov <krastan.draganov@gmail.com>
2020-11-15 14:53:00 +0200

    Debugging BG Autumn B 2008 problem Rings

--- BG/Autumn/B/2008/rings.cpp
+++ BG/Autumn/B/2008/rings.cpp
@@ -1,45 +1,48 @@
-//Only 90/100 points
-
 #include<iostream>
 #include<vector>
 #include<queue>
-#include<cstring>
 
 #define endl '\n'
 
 using namespace std;
 
 const int MAXN=1e3+3;
-int dist[2][MAXN];
 vector<int> graph[MAXN];
 
-void calc_bfs(int startv, int ind){
+vector<int> calc_bfs(int startv, int n){
     queue<int> bfs;
     bfs.push(startv);
-    dist[ind][startv]=0;
+
+    vector<int> dist(n, -1);
+    dist[startv]=0;
+    
     while(!bfs.empty()){
-        int curr=bfs.front();
+        int currv=bfs.front();
         bfs.pop();
-        for(int nextv : graph[curr]){
-            if(dist[ind][nextv]==-1){
-                dist[ind][nextv]=dist[ind][curr]+1;
+        
+        for(int nextv : graph[currv]){
+            if(dist[nextv]==-1){
+                dist[nextv]=dist[currv]+1;
                 bfs.push(nextv);
             }
         }
     }
+
+    return dist;
 }
 
-int calc_res(int a, int b, int n){
-    memset(dist,-1,sizeof(dist));
-    calc_bfs(a,0);
-    calc_bfs(b,1);
-    int ans=0;
+int query(int a, int b, int n){
+    vector<int> dist_a=calc_bfs(a, n);
+    vector<int> dist_b=calc_bfs(b, n);
+    
+    int res=0;
     for(int i=0;i<n;++i){
-        if(i!=a and i!=b and dist[0][i]+dist[1][i]!=dist[0][b]){
-            ++ans;
+        if(dist_a[i]+dist_b[i]!=dist_a[b]){
+            ++res;
         }
     }
-    return ans;
+
+    return res;
 }
 
 int main(){
@@ -62,7 +65,7 @@ int main(){
         cin>>x>>y;
         --x;
         --y;
-        cout<<calc_res(x,y,n)<<" ";
+        cout<<query(x, y, n)<<" ";
     }
     cout<<endl;
 return 0;
