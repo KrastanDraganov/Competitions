0510580762ee18ca608252ef1b9644cca4f34576
Krastan Draganov <krastan.draganov@gmail.com>
2020-11-16 23:57:06 +0200

    Refactoring code of IATI Junior 2018 problem Trap

--- IATI/Junior/2018/trap.cpp
+++ IATI/Junior/2018/trap.cpp
@@ -1,3 +1,5 @@
+// Slow for first 26 numbers
+
 #include<iostream>
 #include<utility>
 #include<set>
@@ -6,48 +8,59 @@
 
 using namespace std;
 
-pair<int,int> dirs[4]={{0,-1},{0,1},{-1,0},{1,0}};
+pair<int,int> dirs[4]={{0,-1}, {0,1}, {-1,0}, {1,0}};
 
-long long sol(int counter, bool check, pair<int,int> curr, set<pair<int,int>> used){
-    used.insert(curr);
-    
+long long sol(int counter, bool is_vertical_line, pair<int,int> curr, set<pair<int,int>>& used){
     if(counter==0){
         for(pair<int,int> add : dirs){
-            int nextx=curr.first+add.first,nexty=curr.second+add.second;
-            if(used.find(make_pair(nextx,nexty))==used.end()){
+            int nextx=curr.first+add.first;
+            int nexty=curr.second+add.second;
+            if(!used.count({nextx, nexty})){
                 return 0;
             }
         }
         return 1;
     }
+
+    used.insert(curr);
     
     long long res=0;
     for(pair<int,int> add : dirs){
-        int nextx=curr.first+add.first,nexty=curr.second+add.second;
-        if(used.find(make_pair(nextx,nexty))==used.end()){
-            if(nexty<curr.second and !check){
-                continue;
-            }else if(nexty>curr.second and !check){
-                res+=sol(counter-1,true,make_pair(nextx,nexty),used);
-            }else{
-                res+=sol(counter-1,check,make_pair(nextx,nexty),used);
-            }
+        int nextx=curr.first+add.first;
+        int nexty=curr.second+add.second;
+        
+        if(used.count({nextx, nexty})){
+            continue;
+        }
+        if(!is_vertical_line and nexty<curr.second){
+            continue;
+        }
+        
+        if(nexty>curr.second and !is_vertical_line){
+            res+=sol(counter-1, true, {nextx, nexty}, used);
+        }else{
+            res+=sol(counter-1, is_vertical_line, {nextx, nexty}, used);
         }
     }
+
+    used.erase(curr);
+
     return res;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    
     cout<<"{";
-    for(int i=1;i<20;++i){
+    for(int i=0;i<26;++i){
         set<pair<int,int>> used;
-        used.insert(make_pair(0,0));
-        if(i>1 and i<26){
+        used.insert({0, 0});
+        
+        if(i>0){
             cout<<",";
         }
-        cout<<sol(i-1,false,make_pair(1,0),used);
+        cout<<sol(i, false, {1, 0}, used);
     }
     cout<<"}\n";
 return 0;
