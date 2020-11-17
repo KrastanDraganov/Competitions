14e99b26d03e0a7e248165ede53177b28bd20925
Krastan Draganov <krastan.draganov@gmail.com>
2020-11-17 17:31:01 +0200

    Speeding up the precompute of IATI Junior 2018 problem Trap

--- IATI/Junior/2018/trap.cpp
+++ IATI/Junior/2018/trap.cpp
@@ -1,67 +1,63 @@
-// Slow for first 26 numbers
-
 #include<iostream>
 #include<utility>
-#include<set>
 
 #define endl '\n'
 
 using namespace std;
 
+const int MAX_LINES=26,LIMIT=50,MAXN=103;
+int counter[MAX_LINES];
+bool used[MAXN][MAXN];
 pair<int,int> dirs[4]={{0,-1}, {0,1}, {-1,0}, {1,0}};
 
-long long sol(int counter, bool is_vertical_line, pair<int,int> curr, set<pair<int,int>>& used){
-    if(counter==0){
-        for(pair<int,int> add : dirs){
-            int nextx=curr.first+add.first;
-            int nexty=curr.second+add.second;
-            if(!used.count({nextx, nexty})){
-                return 0;
-            }
-        }
-        return 1;
+void calc_solutions(int lines, bool is_vertical_line, int currx, int curry){
+    if(lines>MAX_LINES){
+        return;
     }
 
-    used.insert(curr);
+    used[currx+LIMIT][curry+LIMIT]=true;
     
-    long long res=0;
+    long long already_used=0;
     for(pair<int,int> add : dirs){
-        int nextx=curr.first+add.first;
-        int nexty=curr.second+add.second;
+        int nextx=currx+add.first;
+        int nexty=curry+add.second;
         
-        if(used.count({nextx, nexty})){
+        if(used[nextx+LIMIT][nexty+LIMIT]){
+            ++already_used;
             continue;
         }
-        if(!is_vertical_line and nexty<curr.second){
+        if(!is_vertical_line and nexty<curry){
             continue;
         }
         
-        if(nexty>curr.second and !is_vertical_line){
-            res+=sol(counter-1, true, {nextx, nexty}, used);
+        if(nexty>curry and !is_vertical_line){
+            calc_solutions(lines+1, true, nextx, nexty);
         }else{
-            res+=sol(counter-1, is_vertical_line, {nextx, nexty}, used);
+            calc_solutions(lines+1, is_vertical_line, nextx, nexty);
         }
     }
 
-    used.erase(curr);
+    if(already_used==4){
+        ++counter[lines-1];
+    }
 
-    return res;
+    used[currx+LIMIT][curry+LIMIT]=false;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
+    used[LIMIT][LIMIT]=true;
+    calc_solutions(1, false, 1, 0);
+
     cout<<"{";
-    for(int i=0;i<26;++i){
-        set<pair<int,int>> used;
-        used.insert({0, 0});
-        
+    for(int i=0;i<MAX_LINES;++i){
         if(i>0){
             cout<<",";
         }
-        cout<<sol(i, false, {1, 0}, used);
+        cout<<counter[i];
     }
-    cout<<"}\n";
+    cout<<"}";
 return 0;
 }
