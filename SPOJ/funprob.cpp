7dda7ffeb92975c590ff42844bac08d766ab6aa8
Krastan Draganov <krastan.draganov@gmail.com>
2021-01-23 18:32:04 +0200

    Debugging SPOJ problem FUNPROB

--- SPOJ/funprob.cpp
+++ SPOJ/funprob.cpp
@@ -14,7 +14,7 @@ int main(){
     int n,m;
 
     while(cin>>n>>m and !(n==0 and m==0)){
-        double res=double(m+1-n)/double(m+1);
+        double res=(n>m ? 0.0 : double(m+1-n)/(m+1));
         cout<<fixed<<setprecision(6)<<res<<endl;
     }
 return 0;
