1d022d28a27b1730e52ce99c9212d9493964f933
Krastan Draganov <krastan.draganov@gmail.com>
2021-03-07 12:37:52 +0200

    Solving SPOJ problem FASTFLOW

---
+++ SPOJ/fastflow.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+#define endl '\n'
+
+using namespace std;
+
+const int MAXN = 5e3 + 3, INF = 1e9 + 3;
+
+struct Edge {
+    int to, flow, capacity, reversed_ind;
+
+    Edge (int _to, int _capacity, int _reversed_ind) {
+        to = _to;
+        flow = 0;
+        capacity = _capacity;
+        reversed_ind = _reversed_ind;
+    }
+};
+
+vector<Edge> graph[MAXN];
+int dist[MAXN], visited[MAXN], edge_ind[MAXN];
+
+void add_edge(int from, int to, int capacity) {
+    graph[from].push_back(Edge(to, capacity, (int) graph[to].size()));
+    graph[to].push_back(Edge(from, capacity, (int) graph[from].size() - 1));
+}
+
+bool is_augmenting_path(int source, int sink, int n) {
+    for (int i = 0; i < n; ++i) {
+        dist[i] = -1;
+    }
+    dist[0] = 0;
+
+    queue<int> bfs;
+    bfs.push(source);
+
+    while (!bfs.empty()) {
+        int currv = bfs.front();
+        bfs.pop();
+
+        if (currv == sink) {
+            break;
+        }
+
+        for (Edge edge : graph[currv]) {
+            if (dist[edge.to] != -1 or (edge.capacity - edge.flow == 0)) {
+                continue;
+            }
+
+            dist[edge.to] = dist[currv] + 1;
+            bfs.push(edge.to);
+        }
+    }
+
+    return dist[sink] != -1;
+}
+
+int dfs(int currv, int min_capacity, int sink) {
+    if (currv == sink) {
+        return min_capacity;
+    }
+
+    visited[currv] = true;
+    for (int& i = edge_ind[currv]; i < (int) graph[currv].size(); ++i) {
+        Edge& edge = graph[currv][i];
+
+        int left_capacity = edge.capacity - edge.flow;
+        if (left_capacity == 0 or (dist[currv] + 1 != dist[edge.to])) {
+            continue;
+        }
+        
+        int next_capacity = min(min_capacity, left_capacity);
+        int curr_flow = dfs(edge.to, next_capacity, sink);
+
+        if (curr_flow == 0) {
+            continue;
+        }
+
+        edge.flow += curr_flow;
+
+        Edge& reversed_edge = graph[edge.to][edge.reversed_ind];
+        reversed_edge.flow -= curr_flow;
+
+        return curr_flow;
+    }
+
+    return 0;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; ++i) {
+        int from, to, capacity;
+        cin >> from >> to >>capacity;
+        add_edge(from - 1, to - 1, capacity);
+    }
+
+    int source = 0, sink = n - 1;
+    
+    long long max_flow = 0;
+    while (is_augmenting_path(source, sink, n)) {
+        for (int i = 0; i < n; ++i) {
+            edge_ind[i] = 0;
+        }
+
+        int curr_flow = 0;
+        do {
+            curr_flow = dfs(source, INF, sink);
+            max_flow += 1ll * curr_flow;
+        } while (curr_flow > 0);
+    }
+
+    cout << max_flow << endl;
+return 0;
+}
