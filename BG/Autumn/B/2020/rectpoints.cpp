ce79a2e6622fe8450bd1b1ae1713fd6b6fdff9e9
Krastan Draganov <krastan.draganov@gmail.com>
2021-03-29 17:20:10 +0300

    Solving BG Autumn B 2020 problem Rectpoints for 100 points

--- BG/Autumn/B/2020/rectpoints.cpp
+++ BG/Autumn/B/2020/rectpoints.cpp
@@ -1,47 +1,110 @@
-// Not solved - 23 points, wrong answer
-
 #include<iostream>
+#include<algorithm>
 
 #define endl '\n'
 
 using namespace std;
 
-const int MAXC=1e3+3;
-int prefix[MAXC][MAXC];
+struct Point {
+    int x, y;
+    int lowest, highest;
 
-int main(){
+    Point() {
+        x = y = lowest = highest = -1;
+    }
+};
+
+const int MAXN = 1e5 + 3;
+Point points[MAXN];
+int tree[4 * MAXN], lazy[4 * MAXN];
+
+bool cmp_by_x(const Point& p1, const Point& p2) {
+    return p1.x < p2.x;
+}
+
+bool cmp_by_y(const Point& p1, const Point& p2) {
+    return p1.y < p2.y;
+}
+
+void push_lazy(int ind) {
+    tree[2 * ind] += lazy[ind];
+    lazy[2 * ind] += lazy[ind];
+    
+    tree[2 * ind + 1] += lazy[ind];
+    lazy[2 * ind + 1] += lazy[ind];
+
+    lazy[ind] = 0;
+}
+
+void update_tree(int ind, int tl, int tr, int l, int r, int val) {
+    if (l > tr or r < tl or l > r) {
+        return;
+    }
+
+    if (l == tl and r == tr) {
+        tree[ind] += val;
+        lazy[ind] += val;
+        return;
+    }
+
+    push_lazy(ind);
+
+    int mid = (tl + tr) / 2;
+    update_tree(2 * ind, tl, mid, l, min(r, mid), val);
+    update_tree(2 * ind + 1, mid + 1, tr, max(l, mid + 1), r, val);
+
+    tree[ind] = max(tree[2 * ind], tree[2 * ind + 1]);
+}
+
+void compress_y(int n, int h) {
+    sort(points, points + n, cmp_by_y);
+
+    int l = 0;
+    for (int r = 0; r < n; ++r) {
+        if (r > 0 and points[r - 1].y == points[r].y) {
+            points[r].lowest = points[r -1].lowest;
+        } else {
+            points[r].lowest = r;
+        }
+
+        while (l < r and points[r].y - points[l].y > h) {
+            points[l++].highest = r - 1;
+        }
+    }
+    
+    while (l < n) {
+        points[l++].highest = n - 1;
+    }
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n,w,h;
-    cin>>n>>w>>h;
-    ++h;
-    ++w;
+    int n, w, h;
+    cin >> n >> w >> h;
 
-    for(int i=0;i<n;++i){
-        int x,y;
-        cin>>x>>y;
-        ++prefix[x][y];
+    for (int i = 0; i < n; ++i) {
+        cin >> points[i].x >> points[i].y;
     }
 
-    int res=0;
-    for(int i=1;i<MAXC;++i){
-        for(int i2=1;i2<MAXC;++i2){
-            prefix[i][i2]+=prefix[i-1][i2]+prefix[i][i2-1]-prefix[i-1][i2-1];
+    compress_y(n, h);
 
-            int points=prefix[i][i2];
-            if(i>=w and i2>=h){
-                points=points-prefix[i-w][i2]-prefix[i][i2-h]+prefix[i-w][i2-h];
-            }else if(i>=w){
-                points-=prefix[i-w][i2];
-            }else if(i2>=h){
-                points-=prefix[i][i2-h];
-            }
-
-            res=max(res, points);
+    sort(points, points + n, cmp_by_x);
+    int res = 0;
+    
+    int l = 0;
+    for (int r = 0; r < n; ++r) {
+        update_tree(1, 0, n - 1, points[r].lowest, points[r].highest, 1);
+        
+        while (l < r and points[r].x - points[l].x > w) {
+            update_tree(1, 0, n - 1, points[l].lowest, points[l].highest, -1);
+            ++l;
         }
+
+        res = max(res, tree[1]);
     }
 
-    cout<<res<<endl;
+    cout << res << endl;
 return 0;
 }
