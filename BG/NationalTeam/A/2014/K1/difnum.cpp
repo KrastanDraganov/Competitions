36472b1553e3ecd1d722f10ff94c293f33bcefee
Krastan Draganov <krastan.draganov@gmail.com>
2021-04-30 14:50:53 +0300

    Solving BG National Team A 2014 K1 problem Difnum for 65 points

--- BG/NationalTeam/A/2014/K1/difnum.cpp
+++ BG/NationalTeam/A/2014/K1/difnum.cpp
@@ -1,4 +1,4 @@
-// Not solved - 0 points, wrong answer
+// Not solved - 65 points, runtime error (wrong answer to query at the middle)
 
 #include<iostream>
 #include<set>
@@ -12,17 +12,18 @@
 using namespace std;
 
 const int MAXN = 5e4 + 3, INF = 1e9;
+const int WITHOUT_ERASE = 0, WITH_ERASE = 1;
 mt19937 generator(1948);
 
 struct Treap {
     int val, priority;
-    int subtree_size;
+    int subtree_size, counter;
     array<Treap*, 2> kids;
     
     Treap(int _val) {
         val = _val;
         priority = uniform_int_distribution<int>(0, INF)(generator);
-        subtree_size = 1;
+        subtree_size = counter = 1;
         kids = {nullptr, nullptr};
     }
 };
@@ -30,20 +31,6 @@ struct Treap {
 int nums[MAXN], nearest_left[MAXN];
 map<int, set<int>> positions;
 Treap* tree[4 * MAXN];
-
-void print_treap(Treap* curr) {
-    if (!curr) {
-        return;
-    }
-
-    cout << curr->val << " ";
-    
-    cout << "l: ";
-    print_treap(curr->kids[0]);
-
-    cout << "r: ";
-    print_treap(curr->kids[1]);
-}
 
 int treap_size(Treap* curr) {
     return curr ? curr->subtree_size : 0;
@@ -54,7 +41,7 @@ void recover(Treap* curr) {
         return;
     }
 
-    curr->subtree_size = 1;
+    curr->subtree_size = curr->counter;
     for (Treap* kid : curr->kids) {
         curr->subtree_size += treap_size(kid);
     }
@@ -99,6 +86,14 @@ array<Treap*, 2> split(Treap* curr, int target) {
 
 Treap* insert_num_treap(Treap* treap, int num) {
     array<Treap*, 2> split_point1 = split(treap, num);
+    array<Treap*, 2> split_point2 = split(split_point1[1], num + 1);
+
+    if (split_point2[0]) {
+        ++split_point2[0]->counter;
+        ++split_point2[0]->subtree_size;
+        
+        return merge_treap(split_point1[0], merge_treap(split_point2[0], split_point2[1]));
+    }
     
     Treap* new_node = new Treap(num);
     return merge_treap(split_point1[0], merge_treap(new_node, split_point1[1]));
@@ -107,7 +102,14 @@ Treap* insert_num_treap(Treap* treap, int num) {
 Treap* erase_num_treap(Treap* treap, int num) {
     array<Treap*, 2> split_point1 = split(treap, num);
     array<Treap*, 2> split_point2 = split(split_point1[1], num + 1);
-
+    
+    if (split_point2[0]->counter > 1) {
+        --split_point2[0]->counter;
+        --split_point2[0]->subtree_size;
+    
+        return merge_treap(split_point1[0], merge_treap(split_point2[0], split_point2[1]));
+    }
+    
     return merge_treap(split_point1[0], split_point2[1]);
 }
 
@@ -120,36 +122,26 @@ int count_smaller_treap(Treap* treap, int target) {
         return count_smaller_treap(treap->kids[0], target);
     }
 
-    return treap_size(treap->kids[0]) + 1 + count_smaller_treap(treap->kids[1], target);
+    return treap_size(treap->kids[0]) + treap->counter + count_smaller_treap(treap->kids[1], target);
 }
 
-void build_tree(int ind, int tl, int tr) {
-    if (tl == tr) {
-        tree[ind] = new Treap(nearest_left[tl]);
-        return;
-    }
-
-    int mid = (tl + tr) / 2;
-    build_tree(2 * ind, tl, mid);
-    build_tree(2 * ind + 1, mid + 1, tr);
-
-    tree[ind] = merge_treap(tree[2 * ind], tree[2 * ind + 1]);
-}
-
+template<int type>
 void tree_update_pos(int ind, int tl, int tr, int pos, int new_value) {
-    tree[ind] = erase_num_treap(tree[ind], nearest_left[pos]);
+    if (type == WITH_ERASE) {
+        tree[ind] = erase_num_treap(tree[ind], nearest_left[pos]);
+    }
     tree[ind] = insert_num_treap(tree[ind], new_value);
-
+    
     if (tl == tr) {
         nearest_left[pos] = new_value;
         return;
     }
-
+    
     int mid = (tl + tr) / 2;
     if (pos <= mid) {
-        tree_update_pos(2 * ind, tl, mid, pos, new_value);
+        tree_update_pos<type>(2 * ind, tl, mid, pos, new_value);
     } else {
-        tree_update_pos(2 * ind + 1, mid + 1, tr, pos, new_value);
+        tree_update_pos<type>(2 * ind + 1, mid + 1, tr, pos, new_value);
     }
 }
 
@@ -163,8 +155,15 @@ int tree_query(int ind, int tl, int tr, int l, int r, int limit) {
     }
 
     int mid = (tl + tr) / 2;
-    return tree_query(2 * ind, tl, mid, l, min(r, mid), limit)
-         + tree_query(2 * ind + 1, mid + 1, tr, max(l, mid + 1), r, limit);
+
+    if (r <= mid) {
+        return tree_query(2 * ind, tl, mid, l, r, limit);
+    } else if (l > mid) {
+        return tree_query(2 * ind + 1, mid + 1, tr, l, r, limit);
+    } else {
+        return tree_query(2 * ind, tl, mid, l, mid, limit)
+             + tree_query(2 * ind + 1, mid + 1, tr, mid + 1, r, limit);   
+    }
 }
 
 int main() {
@@ -178,13 +177,11 @@ int main() {
         cin >> nums[i];
 
         set<int> &curr = positions[nums[i]];
-        nearest_left[i] = (curr.empty() ? -1 : *curr.rbegin());
-        tree_update_pos(1, 0, n - 1, i, nearest_left[i]);
+        int value = (curr.empty() ? -1 : *curr.rbegin());
+        tree_update_pos<WITHOUT_ERASE>(1, 0, n - 1, i, value);
 
         curr.insert(i);
     }
-
-    print_treap(tree[1]);
 
     int q, m;
     cin >> q >> m;
@@ -193,46 +190,52 @@ int main() {
     for (int i = 0; i < q; ++i) {
         int type;
         cin >> type;
-
+        
         if (type == 0) {
             int l, r;
             cin >> l >> r;
-            l ^= p;
-            r ^= p;
-
+            if (m == 1) {
+                l ^= p;
+                r ^= p;
+            }
+        
             int res = tree_query(1, 0, n - 1, l, r, l);
             cout << res << endl;
-
-            p ^= res;
+        
+            p = res;
             continue;
         }
-
+        
         int pos, new_value;
         cin >> pos >> new_value;
-        pos ^= p;
-        new_value ^= p;
+        if (m == 1) {
+            pos ^= p;
+            new_value ^= p;
+        }
 
         set<int> &prev_num = positions[nums[pos]];
         set<int> &new_num = positions[new_value];
-
-        auto it = prev_num.lower_bound(pos);
-        ++it;
+        
+        // Update the #nearest_left value of the num that is located rightmost to #pos and is equal to #nums[pos]
+        auto it = prev_num.upper_bound(pos);
         if (it != prev_num.end()) {
-            tree_update_pos(1, 0, n - 1, *it, nearest_left[pos]);
+            tree_update_pos<WITH_ERASE>(1, 0, n - 1, *it, nearest_left[pos]);
         }
-
+        
+        // Update the #nearest_left value of the num that is located rightmost to #pos and is equal to #new_value
         it = new_num.upper_bound(pos);
         if (it != new_num.end()) {
-            tree_update_pos(1, 0, n - 1, *it, pos);
+            tree_update_pos<WITH_ERASE>(1, 0, n - 1, *it, pos);
         }
-
-        if (it != new_num.begin()) {
+        
+        // Update the #nearest_left value of the new num located at #pos
+        if (!new_num.empty() and it != new_num.begin()) {
             --it;
-            tree_update_pos(1, 0, n - 1, pos, *it);
+            tree_update_pos<WITH_ERASE>(1, 0, n - 1, pos, *it);
         } else {
-            tree_update_pos(1, 0, n - 1, pos, -1);
+            tree_update_pos<WITH_ERASE>(1, 0, n - 1, pos, -1);
         }
-
+        
         nums[pos] = new_value;
         prev_num.erase(pos);
         new_num.insert(pos);
