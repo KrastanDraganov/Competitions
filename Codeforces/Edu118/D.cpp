abe8801d6f6544cb24e72b3eb20d4e4f6678f4e9
Krastan Draganov <krastan.draganov@gmail.com>
2021-12-14 23:54:11 +0200

    Debugging Codeforces Educational 118 problem D (Accepted)

--- Codeforces/Edu118/D.cpp
+++ Codeforces/Edu118/D.cpp
@@ -1,5 +1,3 @@
-// Not solved - wrong answer
-
 #include <iostream>
 #include <vector>
 
@@ -25,36 +23,31 @@ void solve() {
         cin >> nums[i];
     }
 
-    // dp[i][0] - number of complete subsequences (from 1 to i)
-    // dp[i][1] - number of incomplete subsequences (from 1 to i - 2 and i)
-    vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+    // dp[i][0] - number of complete subsequences (from 0 to x) with mex i
+    // dp[i][1] - number of incomplete subsequences (from 0 to x - 2 and x and x - 2 etc) with mex i
+    vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+    dp[0][0] = 1;
 
     for (int i = 0; i < n; ++i) {
         int curr = nums[i];
 
-        add_mod(dp[curr][1], dp[curr][1]);
-        if (curr == 0) {
-            add_mod(dp[curr][1], 1);
-        }
-        if (curr >= 1) {
-            add_mod(dp[curr][1], dp[curr - 1][1]);
-        }
+        add_mod(dp[curr + 1][0], dp[curr + 1][0]);
+        add_mod(dp[curr + 1][0], dp[curr][0]);
 
-        add_mod(dp[curr][0], dp[curr][0]);
-        if (curr == 1) {
-            add_mod(dp[curr][0], 1);
-        }
-        if (curr >= 2) {
-            add_mod(dp[curr][0], dp[curr - 2][1]);
+        add_mod(dp[curr + 1][1], dp[curr + 1][1]);
+        if (curr - 1 >= 0) {
+            add_mod(dp[curr - 1][1], dp[curr - 1][1]);
+            add_mod(dp[curr - 1][1], dp[curr - 1][0]);
         }
     }
 
     int res = 0;
-    for (int i = 0; i <= n; ++i) {
+    for (int i = 0; i <= n + 1; ++i) {
         for (int type = 0; type < 2; ++type) {
             add_mod(res, dp[i][type]);
         }
     }
+    add_mod(res, -1 + MOD);
 
     cout << res << endl;
 }
@@ -71,5 +64,3 @@ int main() {
     }
 return 0;
 }
-
-
